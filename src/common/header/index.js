4076d1d4c53e5dd0fda4b86c3d6cbcfb2362e316
zhangyufan <zhangyufanzyf@qq.com>
2019-06-04 17:17:07 +0800

    代码优化~

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -22,7 +22,9 @@ import {
 class Header extends Component {
 
   getListArea() {
-    if (this.props.focused) {
+    const { focused, list } = this.props;
+
+    if (focused) {
       return (
         <SearchInfo>
           <SearchInfoTitle>
@@ -31,7 +33,7 @@ class Header extends Component {
           </SearchInfoTitle>
           <SearchInfoList>
             {
-              this.props.list.map((item) => {
+              list.map((item) => {
                 return (
                   <SearchInfoItem key={item}>{item}</SearchInfoItem>
                 )
@@ -46,6 +48,8 @@ class Header extends Component {
   }
 
   render() {
+    const { focused, handleInputFocus, handleInputBlur } = this.props;
+
     return (
       <HeaderWrapper>
         <Logo />
@@ -56,14 +60,14 @@ class Header extends Component {
           <NavItem className="right">Aa</NavItem>
           <SearchWrapper>
             <CSSTransition
-              in={this.props.focused}
+              in={focused}
               timeout={200}
               className="slide"
             >
               <NavSearch
-                className={this.props.focused ? 'focused' : ''}
-                onFocus={this.props.handleInputFocus}
-                onBlur={this.props.handleInputBlur}
+                className={focused ? 'focused' : ''}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
               ></NavSearch>
             </CSSTransition>
             <Sous></Sous>
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -9,16 +9,14 @@ const defaultState = fromJS({
 
 
 export default (state = defaultState, action) => {
-  if (action.type === constants.SEARCH_FOCUS) {
-    // immutable对象的set方法，会结合之前immutable对象的值和设置的值，返回一个全新的对象
-    return state.set('focused', true);
+  switch (action.type) {
+    case constants.SEARCH_FOCUS :
+      return state.set('focused', true); // immutable对象的set方法，会结合之前immutable对象的值和设置的值，返回一个全新的对象
+    case constants.SEARCH_BLUR :
+      return state.set('focused', false);
+    case constants.CHANGE_LIST :
+      return state.set('list', action.data);
+    default :
+      return state
   }
-  if (action.type === constants.SEARCH_BLUR) {
-    return state.set('focused', false);
-  }
-  if (action.type === constants.CHANGE_LIST) {
-    return state.set('list', action.data);
-  }
-
-  return state
 }
