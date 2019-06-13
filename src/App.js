17a8b9d2cdd1d12d523cc33b082424e91d0339b0
zhangyufan <zhangyufanzyf@qq.com>
2019-06-13 19:22:52 +0800

    路由1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from  './common/header/index.js'
 import GrobalStyle from './style';
 import store from './store'
@@ -9,7 +10,16 @@ function App() {
   return (
     <Fragment>
       <Provider store={store}>
-        <Header></Header>
+        <div>
+          <Header></Header>
+          {/* 此处使用路由 */}
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact render={() => <div>home</div>}></Route>
+              <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
       <GrobalStyle/>
     </Fragment>
